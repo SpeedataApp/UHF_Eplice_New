c068ec66d547475d05083c3404c6e7509dece5ee
lzfengluo <zhichao.zhang@speedatagroup>
2019-04-08 15:53:56 +0800

    zzc:更新jar包和so库

--- src/com/speedata/uhf/MainActivity.java
+++ src/com/speedata/uhf/MainActivity.java
@@ -101,7 +101,7 @@ public class MainActivity extends Activity implements OnClickListener {
         super.onStop();
         try {
             if (iuhfService != null) {
-                iuhfService.CloseDev();
+                iuhfService.closeDev();
                 //断点后选卡操作会失效，需要重新选卡（掩码）
                 current_tag_epc = null;
                 Cur_Tag_Info.setText("");
@@ -162,7 +162,7 @@ public class MainActivity extends Activity implements OnClickListener {
      * @return
      */
     private boolean openDev() {
-        if (iuhfService.OpenDev() != 0) {
+        if (iuhfService.openDev() != 0) {
             Cur_Tag_Info.setText("Open serialport failed");
             new AlertDialog.Builder(this).setTitle(R.string.DIA_ALERT).setMessage(R.string.DEV_OPEN_ERR).setPositiveButton(R.string.DIA_CHECK, new DialogInterface.OnClickListener() {
 
